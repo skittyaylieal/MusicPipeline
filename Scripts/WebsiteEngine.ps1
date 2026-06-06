2684888c57ab2c80f57da76f9bccf3e7d54f6767
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-06-06 19:06:28 +0100

    Fixed + supposed to be *

--- Scripts/WebsiteEngine.ps1
+++ Scripts/WebsiteEngine.ps1
@@ -326,7 +326,7 @@ function Invoke-HotReload {
 # -----------------------------------------------------------------
 $Port = 49152
 $Listener = New-Object System.Net.HttpListener
-$Listener.Prefixes.Add("http://+:$Port/")
+$Listener.Prefixes.Add("http://*:$Port/")
 
 try {
     $Listener.Start()
