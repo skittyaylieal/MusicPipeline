deeb2a45d9e768aa333f1468432227f06c32a9b8
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-06-08 19:31:29 +0100

    Fixed

--- Scripts/Download.ps1
+++ Scripts/Download.ps1
@@ -133,7 +133,7 @@ $SanitizedURLs | ForEach-Object -Parallel {
             "-o", $using:OutputTemplate,
             "--cache-dir", $using:LocalCacheDir,
             "--js-runtime", "deno",
-            '--extractor-args', 'youtube:player_client=web_music,android_music'
+            "--extractor-args", "youtube:player_client=web_music,web,ios,android,mweb,android_vr",
             "-f", "ba[ext=m4a]/ba",
             "--download-archive", $using:LocalActiveHistoryLog, 
             "--ignore-errors",
