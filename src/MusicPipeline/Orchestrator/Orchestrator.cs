f0040ee5072d1c5eabbac70599e11ab8a65e8df1
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-02 16:39:35 +0100

    Made c# project and started some minor work on it

---
+++ src/MusicPipeline/Orchestrator/Orchestrator.cs
@@ -0,0 +1,11 @@
+using MusicPipeline.Tools.LogEngine;
+namespace MusicPipeline.Orchestrator;
+
+public class Orchestrator
+{
+    // Need tools
+    static void Main()
+    {
+        LogEngine.Out("Test")
+    }
+}
---
+++ src/MusicPipeline/Tools/LogEngine.cs
@@ -0,0 +1,24 @@
+namespace MusicPipeline.Tools.LogEngine;
+
+public class LogEngine
+{
+    // Logs to the file
+    // Takes ASCII codes
+    // Takes the message
+    // Takes the profile file? or just the log file
+
+    static void Out(string LogFile, string Message, string Style = "1;36") 
+    {
+        /*ArgumentException.ThrowIfNullOrEmpty(LogFile);
+        ArgumentException.ThrowIfNullOrEmpty(Message);
+        ArgumentException.ThrowIfNullOrEmpty(Style);
+        if (!File.Exists(LogFile)) {
+            throw new ArgumentException("Log file must exist", nameof(Profiler.LogFile));
+        }*/
+
+        string Contents = File.ReadAllText(LogFile);
+        Contents += Message;
+        File.WriteAllText(LogFile, Contents);
+
+    }
+}
