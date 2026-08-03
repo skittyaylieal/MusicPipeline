7ffbb56169cbd3ae7e41a2fb752f0d4b1bd2d100
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-03 21:36:54 +0100

    added reference

--- src/MusicPipeline/Identifier/IdentifierManager.cs
+++ src/MusicPipeline/Identifier/IdentifierManager.cs
@@ -3,5 +3,103 @@ namespace MusicPipeline.SongIdentifiers;
 
 public class IdentifierFile 
 {
-	public required Identifier[] Tracks
+	public required SongIdentifier[] Tracks;
+
 }
+
+
+//SAMPLE CONTENT:
+
+/*
+   {
+      "album": "So Good",
+      "hasLrc": true,
+      "title": "So Good (GOLDHOUSE Remix)",
+      "artist": "Zara Larsson, Ty Dolla $ign",
+      "type": "M4A",
+      "isInstrumental": false,
+      "id": "232da366ce49a7eaffe7dcc9db8a1447",
+      "sizeMb": 10.47
+    },
+    {
+      "album": "So Good",
+      "hasLrc": true,
+      "title": "So Good",
+      "artist": "Zara Larsson, Ty Dolla $ign",
+      "type": "M4A",
+      "isInstrumental": false,
+      "id": "691469811d599f274c347510f245ca49",
+      "sizeMb": 8.06
+    },
+    {
+      "album": "Midnight Sun： Girls Trip",
+      "hasLrc": true,
+      "title": "Hot & Sexy (Girls Trip)",
+      "artist": "Zara Larsson, Tyla",
+      "type": "M4A",
+      "isInstrumental": false,
+      "id": "1c1a00487295f6931a9521f91b9b07a1",
+      "sizeMb": 10.51
+    },
+    {
+      "album": "Can't Tame Her",
+      "hasLrc": true,
+      "title": "Can't Tame Her (VIZE Remix)",
+      "artist": "Zara Larsson, VIZE",
+      "type": "M4A",
+      "isInstrumental": false,
+      "id": "b7c2dce691fc6495ccdf8419a3c4729e",
+      "sizeMb": 8.36
+    },
+    {
+      "album": "So Good",
+      "hasLrc": true,
+      "title": "Sundown",
+      "artist": "Zara Larsson, Wizkid",
+      "type": "M4A",
+      "isInstrumental": false,
+      "id": "a4e42b284ba1804f90439e60790c26db",
+      "sizeMb": 9.76
+    },
+    {
+      "album": "Poster Girl",
+      "hasLrc": true,
+      "title": "Talk About Love",
+      "artist": "Zara Larsson, Young Thug",
+      "type": "M4A",
+      "isInstrumental": false,
+      "id": "fe395885c2cf34a5d4fd3f2659492ce9",
+      "sizeMb": 10.06
+    },
+    {
+      "album": "Talk About Love",
+      "hasLrc": true,
+      "title": "Talk About Love",
+      "artist": "Zara Larsson, Young Thug",
+      "type": "M4A",
+      "isInstrumental": false,
+      "id": "737699c62f9db6d9a4587ae9587b4870",
+      "sizeMb": 9.93
+    },
+    {
+      "album": "Icarus Falls",
+      "hasLrc": true,
+      "title": "Dusk Till Dawn (Radio Edit)",
+      "artist": "ZAYN, Sia",
+      "type": "M4A",
+      "isInstrumental": false,
+      "id": "919edc787b917743c35a27b3ded7862b",
+      "sizeMb": 11.09
+    }
+  ],
+  "lrcCount": 12771,
+  "alerts": [
+    {
+      "type": "warning",
+      "fixAction": "gitpull",
+      "message": "Repository Update Available: Changes pushed from Mac are ready."
+    }
+  ]
+}
+
+*/
