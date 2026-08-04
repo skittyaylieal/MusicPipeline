fc7ea571b065a6019aca6d42b5a421ba77cd4b64
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-04 18:19:10 +0100

    Testing

--- src/MusicPipeline/Program.cs
+++ src/MusicPipeline/Program.cs
@@ -7,6 +7,6 @@ using System.Text.Json;
 var Options = new JsonSerializerOptions { WriteIndented = true };
 List<Profile> TestProfiles = new List<Profile>(){DefaultProfiles.DefaultProfile, new Profile(), DefaultProfiles.ErrorProfile};
 ProfileFile TestProfileFile = new ProfileFile(TestProfiles);
-string TestJsonString = JsonSerializer.Serialize(DefaultProfiles.DefaultProfile, Options);
+string TestJsonString = JsonSerializer.Serialize(TestProfileFile, Options);
 string TestJsonFileName = @"C:\MusicTools\MusicPipeline\Sandbox\TestJsonFile.json";
 File.WriteAllText(TestJsonFileName, TestJsonString);
