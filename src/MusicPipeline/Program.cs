4c9faf86a9bd9e783d97f32883f6c500612e554d
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-21 19:52:58 +0100

    Removed test code

--- src/MusicPipeline/Program.cs
+++ src/MusicPipeline/Program.cs
@@ -2,24 +2,7 @@
 using MusicPipeline.Orchestrator;
 // using MusicPipeline.Profiles;
 // using System.Text.Json;
-using System.IO.Hashing;
-using System.Text;
-
-string firstString = "Hello, World!";
-string secondString = "Another string!";
-
-// Convert strings to byte arrays
-byte[] firstBytes = Encoding.UTF8.GetBytes(firstString);
-byte[] secondBytes = Encoding.UTF8.GetBytes(secondString);
-
-// Hash the first string using the static method
-ulong firstHash = XxHash64.Hash(firstBytes);
-
-// Hash the second string using the static method
-ulong secondHash = XxHash64.Hash(secondBytes);
-
-
-//Orchestrator.Start();
+Orchestrator.Start();
 
 
 // Working!
