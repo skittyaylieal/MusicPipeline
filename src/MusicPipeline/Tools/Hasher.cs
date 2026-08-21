f481d5347130887d12856e3f17fd1c4fe0d022e2
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-21 19:52:01 +0100

    using official .net hashing seems to work

--- src/MusicPipeline/Tools/Hasher.cs
+++ src/MusicPipeline/Tools/Hasher.cs
@@ -1,6 +1,5 @@
 using System.Text;
-using HashifyNet;
-using HashifyNet.Algorithms.XxHash;
+using System.IO.Hashing;
 namespace MusicPipeline.Tools.Hasher;
 
 public static class Hasher
@@ -8,12 +7,10 @@ public static class Hasher
 	public static UInt64 GetHashForSong(string Title, string Artist, string Album = "NONE")
 	{
 		string str = $"{Title}-{Artist}-{Album}";
-		// HEAVILY MODIFIED FROM DemarcPoint on Stack Overflow to use Hashify and xxHash64
-		// https://stackoverflow.com/a/35416167/22942130
-
-		XxHash XxHash3 = HashFactory<XxHash>.Create();
-		// Compute 64bit hash
-		var result = xxHash3.ComputerHash(Encoding.UTF8.GetBytes(str));
-		return BitConverter.ToUInt64(result.Hash, 0);
+		var strBytes = Encoding.UTF8.GetBytes(str);
+		return XxHash64.Hash(BitConverter.ToUInt64(strBytes));
 	}
 }
+
+// NOT WORKING
+// TEST STUFF
