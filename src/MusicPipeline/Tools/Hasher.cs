614f7a86af4ba048e835a353a19741eedb7a3570
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-21 19:24:32 +0100

    didnt like noncryptographic

--- src/MusicPipeline/Tools/Hasher.cs
+++ src/MusicPipeline/Tools/Hasher.cs
@@ -10,7 +10,7 @@ public static class Hasher
 		// HEAVILY MODIFIED FROM DemarcPoint on Stack Overflow to use Hashify and xxHash64
 		// https://stackoverflow.com/a/35416167/22942130
 
-		var xxHash64 = HashFactory.NonCryptographic.xxHash;
+		var xxHash64 = HashFactory.Cryptographic.xxHash;
 		// Compute 64bit hash
 		var result = xxHash64.ComputerHash(Encoding.UTF8.GetBytes(str));
 		return BitConverter.ToUInt64(result.Hash, 0);
