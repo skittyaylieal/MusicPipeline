633c7b6bc98f5981180581b12cc86c29205a629e
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-25 19:30:31 +0100

    stuff

--- src/MusicPipeline/Pipeline/Download/Download.cs
+++ src/MusicPipeline/Pipeline/Download/Download.cs
@@ -7,7 +7,7 @@ namespace MusicPipeline.Pipeline;
 
 class Downloader
 {
-	private Profile activeProfile = "Null";
+	private Profile? activeProfile = null;
 	private string logFile = "Null";
 	private string backupDir = "Null";
 	private string YTDLPPath = "Null";
@@ -22,9 +22,9 @@ class Downloader
 	private int maxDownloadThreads = 0;
 	private bool cleanSweep = false;
 
-	public async Task<Result>Download(string profileFile)
+	public async Task<Result> Download(string profileFile)
 	{
-		activeProfile = ProfileManager.LoadActiveProfile(profileFile);
+		activeProfile = await ProfileManager.LoadActiveProfile(profileFile);
 		logFile = activeProfile.DiagLogFile;
 		backupDir = activeProfile.BackupDir;
 		YTDLPPath = activeProfile.YTDLPExe;
@@ -80,16 +80,17 @@ class Downloader
 		But I'm not 100% sure how the ternary operator works
 		*/
 
-		Parallel.For(0, maxDownloadThreads, DownloadThread);
+		Parallel.For(0, maxDownloadThreads, async i => await DownloadThread(i));
 
 	}
 
-	private async Task<void> DownloadThread(int index)
+	private async Task DownloadThread(int index)
 	{
 		int? colourCode = null;
+		// I know this isn't technically the same order as the original but the testing only has one playlist and I prefer the peach colour. Sue me.
 		switch (index) {
 			case 1:
-				colourCode = 51; // Cyan
+				colourCode = 217; // Peach
 				break; 
 			case 2:
 				colourCode = 201;  // Magenta
@@ -104,14 +105,51 @@ class Downloader
 				colourCode = 22; // Forest
 				break; 
 			case 6:
-				colourCode = 217; // Peach
+				colourCode = 51; // Cyan
 				break;
+			// TODO: add more cases by looking through https://color-palette.hexdocs.pm/ansi_color_codes.html once necessary.
 		}
 
 		string errorLogPath = $@"{configDir}playlist${index}_run_errors.txt";
 		if (File.Exists(errorLogPath)) {File.Delete(errorLogPath);}
 
+		await LogEngine.Out(logFile, $"Processing Playlist URL: {playlists[index]}", "Downloader", colourCode);
 
+
+		/*
+		args in progress
+						"--no-colors
+				--verbose
+				--newline
+				--sleep-interval $using:LocalSleepInterval
+				--max-sleep-interval $using:LocalMaxSleepInterval
+				--sleep-requests $using:LocalSleepRequests
+				--embed-thumbnail
+				--convert-thumbnails jpg
+				--ppa EmbedThumbnail+ffmpeg_o:-vf crop=ih:ih
+				--embed-metadata
+				--parse-metadata title:%(artist)s - %(title)s
+				--parse-metadata uploader:%(artist)s
+				--no-keep-video
+				--force-overwrites
+				--cookies $using:LocalCookiePath
+				-P {LocalBackupDir}
+				-o {OutputTemplate}
+				--cache-dir {LocalCacheDir}
+				--geo-bypass
+				--js-runtime deno
+				--extractor-args youtube:player_js_variant=tv
+				-f bestaudio/best
+				--extract-audio
+				--audio-format m4a
+				--audio-quality 0
+				--download-archive {LocalActiveHistoryLog}
+				--ignore-errors
+				--no-abort-on-error
+				--legacy-server-connect
+				--socket-timeout 30
+				{PlaylistURL}"
+			*/
 
 	}
 }
