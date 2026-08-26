45e132e0bd68e0a516e7724a71b71ec767f10811
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-26 18:48:41 +0100

    WHY WON'T YOU WORKK

--- src/MusicPipeline/Pipeline/Download/Download.cs
+++ src/MusicPipeline/Pipeline/Download/Download.cs
@@ -51,7 +51,7 @@ class Downloader
 		DateTime start = DateTime.UtcNow;
 
 		if (Directory.Exists(configDir)) {
-			IEnumerable<string> allSubFiles = Directory.EnumerateFiles(configDir, @"run_errors_playlist\d+\.txt", SearchOption.AllDirectories);
+			IEnumerable<string> allSubFiles = Directory.EnumerateFiles(configDir, "run_errors_playlist\\d+\\.txt", SearchOption.AllDirectories);
 			foreach (string file in allSubFiles) {
 				LogEngine.Out(logFile, $"File found {file}", "Downloader", DefaultColours.Debug);
 				// Temporary debug to check that it's finding the right files
