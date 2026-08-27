199f697791a629d71942238ec23175a92425c9bb
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-27 12:09:26 +0100

    new()

--- src/MusicPipeline/Pipeline/Download/Download.cs
+++ src/MusicPipeline/Pipeline/Download/Download.cs
@@ -27,8 +27,8 @@ class Downloader
 	private int sleepRequests = 0;
 	private int maxDownloadThreads = 0;
 	private bool cleanSweep = false;
-	private DateTime start = new DateTime();
-	private List<Result> res = new;
+	private DateTime start = new();
+	private List<Result> res = new();
 
 	public async Task<List<Result>> Download(string profileFile)
 	{
