b8cac7ae6b2373beebc660b8cb3bc7291757eacd
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-08-29 13:53:26 +0100

    T Ƨ Ǝ T  ວ O ⅃

--- src/MusicPipeline/Tools/LogEngine.cs
+++ src/MusicPipeline/Tools/LogEngine.cs
@@ -47,9 +47,9 @@ public class LogEngine
 		if (!File.Exists(LogFile)) {
 			throw new ArgumentException("Log file must exist", nameof(Profiler.LogFile));
 		}*/
-
-		if (logFile == "Null" & logFileParam != null) {logFile = logFileParam;}
-		else {return;}
+		Console.WriteLine("Log Test-1");
+		if (logFile == "Null" & logFileParam != null) {logFile = logFileParam;Console.WriteLine("Log Test-0.5");}
+		else {Console.WriteLine("Log Test0");return;}
 		Console.WriteLine("Log Test");
 
 		switch (style) {
