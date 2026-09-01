8f92a0db4ffc3389dd04982b029c31c3ba3bbf4d
KillerKing333 <killerking333@gmail.com>
2026-08-31 19:31:14 -0500

    errr wait that's not all the files wtf?

--- src/MusicPipeline/Tools/SafetyChecks.cs
+++ src/MusicPipeline/Tools/SafetyChecks.cs
@@ -32,7 +32,14 @@ public class SafetyCheck
 					string temp1 = p.Name;
 					await profile.LogEngine.Out(p.ToString() ?? "p was null lol", "SafetyChecker");
 					Object? temp2 = p.GetValue(profile);
-					Object? temp3 = p.GetValue(fields[i]);
+                    /* 
+						ok, so the line below is throwing the error.
+						the reason is because it doesn't make any sense :)
+						temp2 makes sense. you're getting the Name property from an instance of Profile, and Profile has a property named Name.
+						temp3 you're trying to get the Name property from an instance of FieldInfo. The FieldInfo class doesn't have a property named Name.
+						What were you wanting temp3 to be? temp2 is already doing something nice.
+					*/
+                    Object? temp3 = p.GetValue(fields[i]);
 					KeyValuePair<string, Object?> temp4 = new(temp1, temp2);
 					KeyValuePair<string, Object?> temp5 = new(temp1, temp3);
 					KeyValuePair<KeyValuePair<string, Object?>, KeyValuePair<string, Object?>> temp6 = new (temp4, temp5);
