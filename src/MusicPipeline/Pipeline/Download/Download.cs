c70e59e816a521c67e757f470f14ce9e927f9a6b
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-09-02 16:10:54 +0100

    Whys it spending so long between each one lol

--- src/MusicPipeline/Pipeline/Download/Download.cs
+++ src/MusicPipeline/Pipeline/Download/Download.cs
@@ -237,7 +237,7 @@ class Downloader
 				while (!((await YTDLPProcess.StandardOutput.ReadLineAsync() ?? await YTDLPProcess.StandardError.ReadLineAsync()) == null)) {
 					string? currentOutput = await YTDLPProcess.StandardOutput.ReadLineAsync();
 					string? currentError = await YTDLPProcess.StandardError.ReadLineAsync();
-					//await l.Out($"Output = {currentOutput}, Error = {currentError}", DefaultColours.Debug);
+					await l.Out($"Output = {currentOutput}, Error = {currentError}", DefaultColours.Debug);
 					if (currentOutput != null && currentError != null) {
 						if (lastOutputLine != currentOutput) {await l.Out(currentOutput, colourCode); lines.Add(currentOutput);}
 						int? errorCode = colourCode;
