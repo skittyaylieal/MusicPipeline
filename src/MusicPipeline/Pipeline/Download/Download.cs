b81870df7bc4102af583ce3287c7ce74894fa92f
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-09-02 14:40:18 +0100

    Manually reading error line now I don't know about adding the redirect in the helper, it prevents user error but makes it a little harder to know what's happenening.

--- src/MusicPipeline/Pipeline/Download/Download.cs
+++ src/MusicPipeline/Pipeline/Download/Download.cs
@@ -233,7 +233,7 @@ class Downloader
 				
 				string? currentLine;
 				List<string> lines = [""];
-				while (!((currentLine = (await YTDLPProcess.StandardOutput.ReadLineAsync())) == null)) {
+				while (!((currentLine = (await YTDLPProcess.StandardOutput.ReadLineAsync()) ?? await YTDLPProcess.StandardError.ReadLineAsync()) == null)) {
 					if (currentLine != null) {
 						await l.Out(currentLine, colourCode);
 						lines.Add(currentLine);
