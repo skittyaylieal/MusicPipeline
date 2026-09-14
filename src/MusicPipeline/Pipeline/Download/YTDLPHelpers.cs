ebf0c43414b6f31a03d231577c435ddc4f8e7e79
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-09-14 17:10:22 +0100

    Debugging

--- src/MusicPipeline/Pipeline/Download/YTDLPHelpers.cs
+++ src/MusicPipeline/Pipeline/Download/YTDLPHelpers.cs
@@ -164,7 +164,7 @@ public class YTDLPHelpers
 				// If the current line is a song decleration
 				inSong = true;
 				Console.WriteLine($"Line = {val}");
-				//Console.WriteLine($"First match value = {Regex.Match(val, _songDeclarePattern).Captures[0].Value}, Regex Match count = {Regex.Match(val, _songDeclarePattern).Count}, ");
+				Console.WriteLine($"First match value = {Regex.Match(val, _songDeclarePattern).Captures[0].Value}, Regex Capture count = {Regex.Match(val, _songDeclarePattern).Captures.Count}, ");
 				songNum = int.Parse(Regex.Match(val, _songDeclarePattern).Captures[0].Value);
 				total = int.Parse(Regex.Match(val, _songDeclarePattern).Captures[1].Value);
 				songs.Add(songNum, [val]);
