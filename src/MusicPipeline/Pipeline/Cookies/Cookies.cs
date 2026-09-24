3af8fdfd3a4c81e38cb2ee2ac4c739f8111996c1
Filip Pybus <filip.pybus2@gmail.com>
2026-09-24 11:03:03 +0100

    Documentation. Debugging

--- src/MusicPipeline/Pipeline/Cookies/Cookies.cs
+++ src/MusicPipeline/Pipeline/Cookies/Cookies.cs
@@ -10,16 +10,30 @@ namespace MusicPipeline.Pipeline;
 class Cookies
 {
 	// Class Colour Code is 112
-	public static async Task<Result> CookieCheck(string ProfileFile)
+	/// <summary>
+	/// Checks cookies with YTDLP asynchronously
+	/// </summary>
+	/// <remarks>
+	/// <para>
+	/// 	Asynchronous method that checks for cookie file, verifies they work.
+	/// </para>
+	/// <para>
+	/// 	Updates YTDLP, runs a --simulate and --quiet ytdlp instance to check the cookies work.
+	/// </para>
+	/// </remarks>
+	/// <returns>Returns a List of Results</returns>
+	/// <param name="profileFile">String, the profile file to get global values from.</param>
+	public static async Task<List<Result>> CookieCheck(string profileFile)
 	{
 		//in C# local variables should start with lower case, camel case.
-		Profiles.Profile activeProfile = await ProfileManager.LoadActiveProfile(ProfileFile);
+		Profiles.Profile activeProfile = await ProfileManager.LoadActiveProfile(profileFile);
 		LogEngine? l = activeProfile.LogEngine;
 		l.user = "Cookies";
 		await l.Out("Profile Done", DefaultColours.Debug);
 		string cookieFile = activeProfile.CookieFile;
 		string YTDLPPath = activeProfile.YTDLPExe;
 		string testURL = activeProfile.CheckURL;
+		List<Result> res = new();
 
 		await l.Out("Variables Done", DefaultColours.Debug);
 
@@ -33,15 +47,17 @@ class Cookies
 
 		if (!File.Exists(cookieFile)){
 			await l.Out("Cookie File could not be found. Please export one.", DefaultColours.Error, true);
-			return CookieDefaults.FileError(false, start);
+			res.Append(CookieDefaults.FileError(false, start));
+			goto Return;
 		}
 		if (!File.Exists(YTDLPPath)){
 			await l.Out("YTDLP Executable could not be found.", DefaultColours.Error, true);
-			return CookieDefaults.FileError(true, start);
+			res.Append(CookieDefaults.FileError(true, start));
+			goto Return;
 		}
 
 		await l.Out("Updating YTDLP");
-		Result updateResult = await Helper.RunSilentAsync(YTDLPPath, "-U", "Cookie Verification", "YTDLPProcess");
+		res.Append(await Helper.RunSilentAsync(YTDLPPath, "-U", "YTDLP Update", "YTDLPProcess"));
 
 		await l.Out("Cookie and YTDLP files located successfully!", DefaultColours.Success, true);
 		await l.Out("Testing cookies on YouTube.");
@@ -52,7 +68,8 @@ class Cookies
 				if (YTDLPProcess is null) {
 					DateTime endError = DateTime.UtcNow;
 					TimeSpan elapsedError = endError - start;
-					return new Result("Cookie Verification", false, elapsedError, "YTDLPProcess is Null");
+					res.Append(new Result("Cookie Verification", false, elapsedError, "YTDLPProcess is Null"));
+					goto Return;
 				}
 				// The cookie check uses --quiet so doesn't have any output
 				// But this is a useful example for other programs
@@ -68,15 +85,21 @@ class Cookies
 				await YTDLPProcess.WaitForExitAsync();
 				DateTime end = DateTime.UtcNow;
 				TimeSpan elapsed = end - start;
-				return new Result("Cookie Verification", true, elapsed);
+				res.Append(new Result("Cookie Verification", true, elapsed));
+				goto Return;
 			}
 		}
 		catch (System.ComponentModel.Win32Exception ex)
 		{
 			DateTime end = DateTime.UtcNow;
 			TimeSpan elapsed = end - start;
-			return new Result("Cookie Verification", false, elapsed, ex.Message);
+			res.Append(new Result("Cookie Verification", false, elapsed, ex.Message));
 		}
+
+
+		Return:
+			// Return the list of results
+			return res;
 
 	}
 }
